3c2334389d5fa77321543a180e53f0e8333a4e28
Carlos R. Ueda <carlos.ueda@gmail.com>
2024-12-01 18:21:17 -0300

    modulo 0001

--- src/main/java/com/devueda/ebook/modulo001/HelloWorld.java
+++ src/main/java/com/devueda/ebook/modulo001/HelloWorld.java
@@ -1,6 +1,6 @@
-package com.devueda.ebook;
+package com.devueda.ebook.modulo001;
 
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
     }
